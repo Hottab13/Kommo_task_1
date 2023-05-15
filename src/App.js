1ad1c67dfaaf0dd07ff429dad6bcadb69b9fe2d8
Hottab13 <alekseev.a@hotmail.com>
2023-05-15 16:07:15 +0300

    0_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import moment from "moment";
 
 function App() {
+  const [second, setSecond] = useState(false);
+  const [timer, setTimer] = useState("hh:mm:ss");
+  const createTimerAnimator = useCallback(
+    (duration) => {
+      setSecond(second - 1);
+      return [
+        ("0" + duration.hours()).slice(-2),
+        ("0" + duration.minutes()).slice(-2),
+        ("0" + duration.seconds()).slice(-2),
+      ].join(":");
+    },
+    [second]
+  );
+  const submitFormTimer = (e) => {
+    if (!second) e.preventDefault();
+    setSecond(e.target.time.value);
+  };
+  useEffect(() => {
+    if (second > 0) {
+      setTimeout(() => {
+        setTimer(createTimerAnimator(moment.duration(second, "seconds")));
+      }, 1000);
+    } else {
+      setTimer("hh:mm:ss");
+      setSecond(false);
+    }
+  }, [createTimerAnimator, second]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={submitFormTimer}>
+        <input placeholder="Seconds" type="number" name="time" />
+        <button>Start</button>
+        <br />
+        <br />
+        <span>{timer}</span>
+      </form>
     </div>
   );
 }
